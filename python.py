7734422b497be36d36f2680babed49bc100b954f
hunterdavidcb <hunter.davidcb@gmail.com>
2021-03-09 09:11:17 +0900

    Update from March 2

--- python.py
+++ python.py
@@ -1,52 +1,32 @@
-#numbers = []
-
-#for x in range(1,11):
-#    numbers.append(x)
-#print(numbers)
-
-
-#for x in range(len(numbers)):
-#    numbers[x] = numbers[x] * 2
-#print(numbers)
-
-
-
-
 import random
 
 chemicals  = ["A","T","C","G"]
 start = "ATG"
 end = ["TAG","TGA","TAA"]
 
-gene = []
-gene.append(start)
-#gene.append(end[random.randint(0,2)])
-#print(gene)
-
-
-#we need a function to make a group of three chemicals
-def make_codon():
-    codon = ""
-    for x in range(3):
-        codon = codon + chemicals[random.randint(0,3)]
-    if codon == start:
-        return make_codon()
-        #print("start")
-    if codon in end:
-        return make_codon()
-    return codon
+class Gene:
+    #we need a function to make a group of three chemicals
+    def make_codon(self):
+        codon = ""
+        for x in range(3):
+            codon = codon + chemicals[random.randint(0,3)]
+        if codon == start:
+            return self.make_codon()
+            #print("start")
+        if codon in end:
+            return self.make_codon()
+        return codon
+    
+    def __init__(self,codon_number):
+        self.codon_list = []
+        self.codon_list.append(start)
+        for x in range(codon_number):
+            self.codon_list.append(self.make_codon())
+        self.codon_list.append(end[random.randint(0,2)])
+        
     
 
-#for x in range(100):
-#    make_codon()
+gene1 = Gene(24)
 
-#print(make_codon())
-#add 10 codons to gene
-for x in range(10):
-    gene.append(make_codon())
-
-#add the end codon
-gene.append(end[random.randint(0,2)])
-
-#show the gene
-print(gene)
+for codon in gene1.codon_list:
+    print(codon)
